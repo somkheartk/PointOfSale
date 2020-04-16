3db865dc9e12b7f44ed4fc649aa80811bf483de7
Somkheart <somkheart.k@gmail.com>
2020-04-17 05:21:02 +0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserComponent } from './user/user.component'
+
+const routes: Routes = [
+
+  { path: 'first-component', component: UserComponent }
 
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
