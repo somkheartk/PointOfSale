bddbd7340ef9c42db907ae7fcfa708702373b9e7
Somkheart <somkheart.k@gmail.com>
2020-04-20 01:13:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 mongoose.connect('mongodb+srv://pos:k1Y6gLecg7h5YR2A@cluster0-mtjby.gcp.mongodb.net/pos?retryWrites=true&w=majority',{useNewUrlParser:true});
+
+var db = mongoose.connection;
+ 
+db.on('error', console.error.bind(console, 'connection error:'));
+ 
+db.once('open', function() {
+    console.log("Connection Successful!");
+});
 const CustomerSchema = new mongoose.Schema({
   name: {
     type: String
@@ -13,8 +21,8 @@ const CustomerSchema = new mongoose.Schema({
     type:String
   }
 });
-const customerModel = mongoose.model("customer", CustomerSchema);
-const customer=new customerModel({name:"somkheart",lastname:"kraisin"})
+const customerModel = mongoose.model("Customer", CustomerSchema);
+const customer=new customerModel({name:"somkheart",lastname:"kraisin55"})
 app.get("/addCustomer",async function(req,res){
   await customer.save();
   res.send(customer);
